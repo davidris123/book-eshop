2d061ac78513d553c9dec48a3a6ddf3ccf267124
Zheann Filip <hello>
2024-09-13 14:18:48 +0200

    Added Dto + ImportAllBooks functionality

---
+++ Eshop.DomainEntities/DTO/BookDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.DomainEntities.DTO
+{
+    public class BookDto
+    {
+        public string? name {  get; set; }
+        public string? description { get; set; }
+        public string? imageurl { get; set; }
+        public int price { get; set; }
+
+    }
+}
--- EshopWebApplication1/Controllers/API/AdminController.cs
+++ EshopWebApplication1/Controllers/API/AdminController.cs
@@ -1,5 +1,7 @@
 ﻿using Eshop.DomainEntities;
+using Eshop.DomainEntities.DTO;
 using Eshop.Service.Interface;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EshopWebApplication1.Controllers.API
@@ -9,10 +11,11 @@ namespace EshopWebApplication1.Controllers.API
     public class AdminController : Controller
     {
         private readonly IOrderService _orderService;
-
-        public AdminController(IOrderService orderService)
+        private readonly IBookService _bookService;
+        public AdminController(IOrderService orderService, IBookService bookService)
         {
             _orderService = orderService;
+            _bookService = bookService;
         }
 
         [HttpGet("[action]")]
@@ -20,11 +23,37 @@ namespace EshopWebApplication1.Controllers.API
         {
             return _orderService.GetAllOrders();
         }
-
+        [HttpGet("[action]")]
+        public List<Book> GetAllBooks()
+        {
+            return _bookService.GetAllBooks();
+        }
         [HttpPost("[action]")]
         public Order GetDetailsForOrder(BaseEntity model)
         {
             return _orderService.GetDetailsForOrder(model);
         }
+
+        [HttpPost("[action]")]
+        public bool ImportAllBooks(List<BookDto> model)
+        {
+            bool status = true;
+
+            foreach (var item in model)
+            {
+               
+                    var book = new Book
+                    {
+                        Name = item.name,
+                        Description = item.description,
+                        ImageUrl = item.imageurl,
+                        Price = item.price
+                    };
+
+                    _bookService.CreateNewBook(book);
+            }
+            return status;
+        }
+
     }
 }
