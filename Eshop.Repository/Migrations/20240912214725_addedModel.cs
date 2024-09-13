51624e40d8466b4d178fbb72da33cc827a3dfd4e
Zheann Filip <hello>
2024-09-13 15:40:38 +0200

    Forgot to add the migrations. so here they are

---
+++ Eshop.Repository/Migrations/20240912214725_addedModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EshopWebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EshopApplicationUserId",
+                table: "Orders",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.CreateTable(
+                name: "FoodPartner",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FoodName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FoodDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FoodImage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    RestaurantId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodPartner", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_EshopApplicationUserId",
+                table: "Orders",
+                column: "EshopApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AspNetUsers_EshopApplicationUserId",
+                table: "Orders",
+                column: "EshopApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AspNetUsers_EshopApplicationUserId",
+                table: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "FoodPartner");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_EshopApplicationUserId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "EshopApplicationUserId",
+                table: "Orders",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+    }
+}
